d5f1329d9091f879a3e5095634cdac72969ca832
Kevin Smith <ksmith@centricle.com>
2026-03-22 00:31:01 -0400

    Add robots.txt and @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fasteners.fyi',
   output: 'static',
+  integrations: [sitemap()],
   adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
